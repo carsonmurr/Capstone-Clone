1adc4581ed38ebdbc8196f934647d92e3ea63a9d
Carson Murray <cam50@email.sc.edu>
2023-12-08 14:30:14 -0500

    Update settings.py

--- Webapp/workplaceWise/settings.py
+++ Webapp/workplaceWise/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 
 # STATIC_URL = 'static/'
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "frontend/static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "frontend/static/frontend/")
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, "frontend/static"),)
 
 
