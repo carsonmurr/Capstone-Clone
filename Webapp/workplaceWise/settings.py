a7fe2ba2bdc8e35aae3049015d54d4916d442894
Carson Murray <cam50@email.sc.edu>
2023-12-08 00:46:34 -0500

    Update settings.py

--- Webapp/workplaceWise/settings.py
+++ Webapp/workplaceWise/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # STATIC_URL = 'static/'
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "fronted/static"),)
 
 
 # Directory where uploaded profile pictures will be saved
