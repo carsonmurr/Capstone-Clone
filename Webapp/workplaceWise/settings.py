23879ece08a91fdf2e97186c62e19cebeae92d9f
Carson Murray <cam50@email.sc.edu>
2023-12-07 22:48:45 -0500

    Update settings.py

--- Webapp/workplaceWise/settings.py
+++ Webapp/workplaceWise/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 import os
 import dj_database_url
 import sys
