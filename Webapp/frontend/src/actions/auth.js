ebb03688a51491a93551e1e3aea97698e4a8181a
Carson Murray <cam50@email.sc.edu>
2023-12-08 13:47:38 -0500

    Update auth.js

--- Webapp/frontend/src/actions/auth.js
+++ Webapp/frontend/src/actions/auth.js
@@ -172,7 +172,7 @@ export const register = ({ first_name, last_name, username, password, email, emp
 
     // Request body with user registration data
     const body = JSON.stringify({ first_name, last_name, username, password, email, employee_id });
-
+    console.log(body)
     // Send a POST request to the register API
     axios.post('/api/auth/register', body, config)
         .then((res) => {
